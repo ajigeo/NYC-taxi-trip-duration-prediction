b6712faa0365cff3f561f5520c8091dadf7f9757
ajigeo <46032638+ajigeo@users.noreply.github.com>
2023-11-14 11:42:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def Input_Output():
         st.balloons()     
     st.success('The estimated trip duration is {} seconds'.format(np.exp(result)-1))
    
-#if __name__ ==  '__main__':
-#    Input_Output()
+if __name__ ==  '__main__':
+    Input_Output()
 
 # https://python.plainenglish.io/build-and-deploy-ml-web-app-in-pythons-streamlit-68428380fac5
